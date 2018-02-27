c3f11f1c092afa9abd03cbf91c825585a51e7b70
Adam Sarfati <adamsarfat@hotmail.fr>
2018-02-27 12:56:15 +0100

    order offer controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,25 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.where(user: current_user)
+  end
+
+  def new
+    @order = Order.new
+    @product = Product.where(params[:product_id])
+    @transporter = Transporter.where(params[:transporter_id])
+  end
+
+  def create
+    order = Order.new(order_params)
+    order.user = current_user
+    @product = Product.where(params[:product_id])
+    order.product = @product
+    @transporter = Transporter.where(params[:transporter_id])
+    if order.save
+      redirect_to orders_path
+    else
+      render :new
+    end
+  end
 end
