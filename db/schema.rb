684ce3a68ee379d019027131473e99f4d63a1b98
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-02-28 14:39:54 +0100

    add a date to the table products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228115711) do
+ActiveRecord::Schema.define(version: 20180228133206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180228115711) do
     t.string "photo"
     t.string "aa"
     t.string "ad"
+    t.datetime "date"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
