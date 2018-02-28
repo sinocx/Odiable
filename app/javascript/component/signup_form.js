b176a45f726cd3977f6b80516038bd522900b7cd
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-02-28 16:40:22 +0100

    starting JS3
    3

--- app/javascript/component/signup_form.js
+++ app/javascript/component/signup_form.js
@@ -1,8 +1,16 @@
 const stepForm = () => {
-  const step = document.getElementById()
-  // ClassList.add('hidden')
+  const step = document.getElementById("step-1");
+
+    element.addEventListener("click"
+      element.classList.add('hidden', (event) => {
+      event.target.classList.add("hidden");
+
+    });
+    }
 }
 
 // const stepBack =
 
 export  { stepForm };
+
+
