51784fa8f7efb27d8354e8b5b49ab5130a37250f
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-02-28 13:02:45 +0100

    Add address to products, and Remove Address to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227164359) do
+ActiveRecord::Schema.define(version: 20180228115711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,6 @@ ActiveRecord::Schema.define(version: 20180227164359) do
     t.bigint "product_id"
     t.date "checkin"
     t.date "checkout"
-    t.string "aa"
-    t.string "ad"
     t.string "uniquekey"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,6 +46,8 @@ ActiveRecord::Schema.define(version: 20180227164359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "aa"
+    t.string "ad"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
