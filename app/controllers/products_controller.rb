64cad54d0f83a662c0afc5b6eb28eebcb5bc947f
vincent <mvincem@gmail.com>
2018-03-01 11:45:37 +0100

    recupere push de adrien

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
+    if params[:query].present?
+      @products = policy_scope(Product).search_by_aa(params[:query])
+    else
     @products = policy_scope(Product).order(created_at: :desc)
-
+    end
   end
 
   def show
