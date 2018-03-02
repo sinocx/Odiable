8e7a1c1e6b0c61f31b4cd24888c807734e773496
David Crenin <david.crenin@gmail.com>
2018-03-02 09:47:26 +0100

    form et composants

--- app/controllers/transporters_controller.rb
+++ app/controllers/transporters_controller.rb
@@ -1,2 +1,23 @@
 class TransportersController < ApplicationController
+
+  def new
+    @transporter = Transporter.new
+    authorize @transporter
+  end
+
+  def create
+    @transporter = Transporter.new(user_params)
+    authorize @transporter
+    if @transporter.save
+       redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:transporter).permit(:car, :capacity, :permit, :cin)
+
+  end
 end
+
