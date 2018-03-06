2afbfb09e71c568c0060d7e8842b28aa16b0ea84
Adam Sarfati <adamsarfat@hotmail.fr>
2018-03-06 10:50:18 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180305175912) do
-=======
 ActiveRecord::Schema.define(version: 20180306093358) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +67,6 @@ ActiveRecord::Schema.define(version: 20180306093358) do
     t.string "category"
     t.bigint "width_id"
     t.datetime "date_delivery"
-
     t.index ["user_id"], name: "index_products_on_user_id"
     t.index ["width_id"], name: "index_products_on_width_id"
   end
