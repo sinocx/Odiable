8b3dea00683c480ac3f31cedcb8ab74166c1a82e
David Crenin <david.crenin@gmail.com>
2018-03-06 19:07:09 +0100

    test

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -49,7 +49,7 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
     map.addMarkers(markers);
     map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
+    map.setZoom(11.5);
 
   }
 }
