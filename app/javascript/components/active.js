b390ede879a5d35e274af38c9c9d46b6b847b39e
vincent <mvincem@gmail.com>
2018-03-06 11:05:03 +0100

    js

--- app/javascript/components/active.js
+++ app/javascript/components/active.js
@@ -1,3 +1,10 @@
+const backColor = document.querySelectorAll(".collection_radio_buttons")
+backColor.forEach( function(element){
+  element.addEventListener('click', (event) => {
+    $(".collection_radio_buttons").removeClass('actives');
+  });
+});
+
 $(document).ready(function(){
  $(".collection_radio_buttons ").click(function(){
    $(this).toggleClass("actives");
