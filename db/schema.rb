c1aafec20946c9d85b436e483016ff88bb9f541e
Adam Sarfati <adamsarfat@hotmail.fr>
2018-03-06 10:45:12 +0100

    miseAjour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180305175912) do
 
   # These are extensions that must be enabled in order to support this database
@@ -67,7 +66,6 @@ ActiveRecord::Schema.define(version: 20180305175912) do
     t.integer "price_cents", default: 0, null: false
     t.string "category"
     t.bigint "width_id"
-    t.date "date_delivery"
     t.index ["user_id"], name: "index_products_on_user_id"
     t.index ["width_id"], name: "index_products_on_width_id"
   end
