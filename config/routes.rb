d5f43c029c5de7a4c482c43b421f67154b10e849
vincent <mvincem@gmail.com>
2018-03-07 19:07:23 +0100

    test 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'errors/not_found'
-  get 'errors/internal_server_error'
-  match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_server_error", :via => :all
 
   devise_for :users
   root to: 'pages#home'
@@ -18,4 +14,6 @@ Rails.application.routes.draw do
   get "/dashboards/product/:product_id/offer/:id/refused", to: "dashboards#refused", as: "refused_offer"
   get "/transporter/sign_up", to: "transporters#new", as: "sign_up_transporter"
   post "transporter/create", to: "transporters#create", as: "registration_session"
+  get "/404", to: "errors#not_found"
+  get "/500", to: "errors#internal_server_error"
 end
